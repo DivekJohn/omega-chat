5888f560914c706ab64f7f1c685ad4a3f1e5a974
divek@zibtek.in <divek@zibtek.in>
2019-04-20 15:32:11 +0530

    fix(fixed: mongo db connection)

--- app/server.js
+++ app/server.js
@@ -1,79 +1,79 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongodb = require('mongodb');
+const MongoClient = require('mongodb').MongoClient;
 const socket = require('socket.io');
 const cors = require('cors');
-const port = 3000;
+const responseTime = require('response-time');
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 let users;
 let count;
 let chatRooms;
 let messagesArray = [];
-
 const app = express();
 
 app.use(bodyParser.json());
+// Enable All CORS Requests
 app.use(cors());
+// Add response time in headers
+app.use(responseTime());
 
-const MongoClient = mongodb.MongoClient;
+const options = {
+    reconnectTries: 3,
+    poolSize: 3,
+  };
 
-// app.use((req, res, next) => {
-//     res.append('Access-Control-Allow-Origin' , 'http://localhost:4200');
-//     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//     res.append("Access-Control-Allow-Headers", "Origin, Accept,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-//     res.append('Access-Control-Allow-Credentials', true);
-//     next();
-// });
+const uri = "mongodb+srv://divek:connecttodb@cluster0-h6ard.gcp.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri,options);
+client.connect((err, Database) => {
+    if(err) {
+        console.log(err);
+        return false;
+    }
+    console.log("Connected to Database");
+    const db = Database.db("Chat_App");
+    users = db.collection("users");
+    chatRooms = db.collection("chatRooms");
+    const server = app.listen(port, () => {
+        console.log("Server started on port " + port + "...");
+    });
+    const io = socket.listen(server);
 
+    io.sockets.on('connection', (socket) => {
+        socket.on('join', (data) => {
+            socket.join(data.room);
+            chatRooms.find({}).toArray((err, rooms) => {
+                if(err){
+                    console.log(err);
+                    return false;
+                }
+                count = 0;
+                rooms.forEach((room) => {
+                    if(room.name == data.room){
+                        count++;
+                    }
+                });
+                if(count == 0) {
+                    chatRooms.insert({ name: data.room, messages: [] }); 
+                }
+            });
+        });
+        socket.on('message', (data) => {
+            io.in(data.room).emit('new message', {user: data.user, message: data.message});
+            chatRooms.update({name: data.room}, { $push: { messages: { user: data.user, message: data.message } } }, (err, res) => {
+                if(err) {
+                    console.log(err);
+                    return false;
+                }
+                console.log("Document updated");
+            });
+        });
+        socket.on('typing', (data) => {
+            socket.broadcast.in(data.room).emit('typing', {data: data, isTyping: true});
+        });
+    });
 
-// MongoClient.connect('mongodb://localhost:27017/Chat_App', (err, Database) => {
-//     if(err) {
-//         console.log(err);
-//         return false;
-//     }
-//     console.log("Connected to MongoDB");
-//     const db = Database.db("Chat_App");
-//     users = db.collection("users");
-//     chatRooms = db.collection("chatRooms");
-//     const server = app.listen(port, () => {
-//         console.log("Server started on port " + port + "...");
-//     });
-//     const io = socket.listen(server);
-
-//     io.sockets.on('connection', (socket) => {
-//         socket.on('join', (data) => {
-//             socket.join(data.room);
-//             chatRooms.find({}).toArray((err, rooms) => {
-//                 if(err){
-//                     console.log(err);
-//                     return false;
-//                 }
-//                 count = 0;
-//                 rooms.forEach((room) => {
-//                     if(room.name == data.room){
-//                         count++;
-//                     }
-//                 });
-//                 if(count == 0) {
-//                     chatRooms.insert({ name: data.room, messages: [] }); 
-//                 }
-//             });
-//         });
-//         socket.on('message', (data) => {
-//             io.in(data.room).emit('new message', {user: data.user, message: data.message});
-//             chatRooms.update({name: data.room}, { $push: { messages: { user: data.user, message: data.message } } }, (err, res) => {
-//                 if(err) {
-//                     console.log(err);
-//                     return false;
-//                 }
-//                 console.log("Document updated");
-//             });
-//         });
-//         socket.on('typing', (data) => {
-//             socket.broadcast.in(data.room).emit('typing', {data: data, isTyping: true});
-//         });
-//     });
-
-// }); 
+}); 
 
 app.get('/', (req, res, next) => {
     res.write('<h1 style=" color: red"> Hi chewtoo ma miss you so much</h1>');
@@ -157,7 +157,4 @@ app.get('/chatroom/:room', (req, res, next) => {
         }
         res.json(chatroom.messages);
     });
-});
-const server = app.listen(process.env.PORT || port, () => {
-    console.log("Magic is on" + process.env.PORT || port + "...");
 });
